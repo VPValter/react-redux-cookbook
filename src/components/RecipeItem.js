04322d8d33ca53218230f01fcec302426cf7e1d3
Vladimir Perić <mgr.art.v.peric@gmail.com>
2021-11-22 00:36:35 +0100

    additional settings for the recipe list

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,4 +1,11 @@
+import { Link } from "react-router-dom";
+
 const RecipeItem = ({ recipe }) => {
+  const truncIngredients = recipe.ingredients.slice(0, 3);
+  const truncInstructions = recipe.instructions.substring(0, 50);
+  const hours = Math.floor(recipe.prepTime / 60);
+  const minutes = recipe.prepTime % 60;
+
   return (
     <tr className='recipe-item'>
       <td className='id'>{recipe.id}</td>
@@ -6,16 +13,26 @@ const RecipeItem = ({ recipe }) => {
       <td>{recipe.source}</td>
       <td>{recipe.ingredients.length}</td>
       <td className='ingredients-list'>
-        {recipe.ingredients.map((item) => (
+        {truncIngredients.map((item, i) => (
           <span key={item.name}>
             {item.name}
-            {', '}
+            {i < truncIngredients.length - 1 && ', '}
           </span>
         ))}
+        {recipe.ingredients.length > 3 && '...'}
       </td>
-      <td>{recipe.instructions}</td>
-      <td>{recipe.prepTime} min</td>
-      <td>view</td>
+      <td>
+        {recipe.instructions.length > 50
+          ? truncInstructions.substring(0, truncInstructions.lastIndexOf(' ')) +
+            '...'
+          : recipe.instructions}
+      </td>
+      <td>
+        {recipe.prepTime > 59
+          ? `${hours} hrs ${minutes} min`
+          : `${recipe.prepTime} min`}
+      </td>
+      <td><Link to={`/show/${recipe.id}`}>view</Link></td>
       <td>delete</td>
     </tr>
   );
