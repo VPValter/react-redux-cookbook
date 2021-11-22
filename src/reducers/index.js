bb00d291129c163b6443dd8955113f1c650c6081
Vladimir Perić <mgr.art.v.peric@gmail.com>
2021-11-22 02:43:32 +0100

    localStorage storing

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,29 +1,26 @@
 const initialState = {
-  // recipes: JSON.parse(localStorage.getItem('storedRecipes')) || [],
-  recipes: [],
-  // recipe:
+  recipes: JSON.parse(localStorage.getItem('storedRecipes')) || [],
+  // recipes: [],
 };
 
 const rootReducer = (state = initialState, action) => {
   const { type, payload } = action;
-  // console.log(payload);
-  // payload &&
 
   switch (type) {
     case 'ADD_RECIPE':
-      // localStorage.setItem(
-      //   'storedRecipes',
-      //   JSON.stringify([...state.recipes, payload])
-      // );
+      localStorage.setItem(
+        'storedRecipes',
+        JSON.stringify([...state.recipes, payload])
+      );
       return {
         // ...state,
         recipes: [...state.recipes, payload],
       };
     case 'DELETE_RECIPE':
-      // localStorage.setItem(
-      //   'storedRecipes',
-      //   JSON.stringify(state.recipes.filter((item) => item.id !== payload))
-      // );
+      localStorage.setItem(
+        'storedRecipes',
+        JSON.stringify(state.recipes.filter((item) => item.id !== payload))
+      );
       return {
         recipes: state.recipes.filter((item) => item.id !== payload),
       };
